e01426dbde9108ab6846aee1eef32f293fce7a58
Agustín Navarro <112596946+Agusunx@users.noreply.github.com>
2026-01-19 17:28:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,12 +13,14 @@ function toggleInfo(id) {
 window.addEventListener('load', () => {
     console.log("Web de Conectando Almas lista.");
     const titulo = document.querySelector('h1');
-    titulo.style.opacity = '0';
-    titulo.style.transform = 'translateY(20px)';
-    titulo.style.transition = 'opacity 1s ease, transform 1s ease';
-    
-    setTimeout(() => {
-        titulo.style.opacity = '1';
-        titulo.style.transform = 'translateY(0)';
-    }, 300);
+    if(titulo) {
+        titulo.style.opacity = '0';
+        titulo.style.transform = 'translateY(20px)';
+        titulo.style.transition = 'opacity 1s ease, transform 1s ease';
+        
+        setTimeout(() => {
+            titulo.style.opacity = '1';
+            titulo.style.transform = 'translateY(0)';
+        }, 300);
+    }
 });
